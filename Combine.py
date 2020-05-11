bd506751cd13acda291c6332d1e2713e1cf0305d
scolour <64021655+scolour@users.noreply.github.com>
2020-05-11 10:38:43 +0900

    Add files via upload

--- Combine.py
+++ Combine.py
@@ -7,8 +7,8 @@ from time import sleep
 
 def initui():
 	# main screen
-	root.title(u"Combine")
-	root.geometry("585x360")
+	root.title(u"Combine Tool")
+	root.geometry("585x365")
 	
 	# input file
 	lblIn.place(x=10, y=10)
@@ -25,18 +25,25 @@ def initui():
 	entSlp.place(x=110, y=60)
 	entSlp.insert(tk.END,"5")
 	
+	# select style
+	lblStl.place(x=10, y=85)
+	rb1l.place(x=105, y=85)
+	rb2l.place(x=200, y=85)
+	rbVar.set(0)
+	
 	# execute button
-	btnGo.place(x=10, y=90)
+	btnGo.place(x=10, y=115)
 	
 	# stop button
-	btnEnd.place(x=10, y=120)
+	btnEnd.place(x=10, y=145)
 	btnEnd['state'] = tk.DISABLED
-	
+
 	# log
-	#txtLog.configure(state='disabled')
-	txtLog.place(x=10, y=155)
+	txtLog.place(x=10, y=180)
 	txtLog['width'] = 80
-	txtLog['height'] = 15
+	txtLog['height'] = 13
+
+
 	
 	root.mainloop()
 
@@ -49,6 +56,8 @@ def run():
 	entIn['state'] = tk.DISABLED
 	entOut['state'] = tk.DISABLED
 	entSlp['state'] = tk.DISABLED
+	rb1l['state'] = tk.DISABLED
+	rb2l['state'] = tk.DISABLED
 
 	if not thread:
 		thread = th.Thread(target=getJsonTxt)
@@ -71,6 +80,8 @@ def stop():
 	entIn['state'] = tk.NORMAL
 	entOut['state'] = tk.NORMAL
 	entSlp['state'] = tk.NORMAL
+	rb1l['state'] = tk.NORMAL
+	rb2l['state'] = tk.NORMAL
 	txtLog.insert(1.0, '---stop---\n')
 
 def getJsonTxt():
@@ -97,6 +108,7 @@ def getJsonTxt():
 			SOURCE = jsnObj['icestats']['source']
 			ARTIST = SOURCE['artist']
 			TITLE  = SOURCE['title']
+
 			if not ARTIST:
 				ARTIST = 'no data'
 			if not TITLE:
@@ -104,12 +116,13 @@ def getJsonTxt():
 			
 			if ARTIST != strArtist or TITLE != strTitle:
 				with open(outPath, 'w', encoding='utf-8') as txtData:
-#					txtData.write('Artist : ' + ARTIST + '\n')
-#					txtData.write('Title : ' + TITLE + '\n')
-					txtData.write(ARTIST + ' - ' + TITLE + '\n')
-					strArtist = ARTIST
-					strTitle = TITLE
-			
+					if rbVar.get() == 0:
+						txtData.write(ARTIST + ' - ' + TITLE + '\n')
+					else:
+						txtData.write(ARTIST + '\n' + TITLE + '\n')
+			strArtist = ARTIST
+			strTitle = TITLE
+
 			now = datetime.datetime.now().isoformat()
 			logStr = now + ' : ' + ARTIST + ' - ' + TITLE + '\n'
 			txtLog.insert(1.0, logStr)
@@ -152,6 +165,10 @@ if __name__=='__main__':
 	btnGo = tk.Button(text=u'GO',width=20,command=run)
 	btnEnd = tk.Button(text=u'STOP',width=20,command=stop)
 	txtLog = tk.Text(root)
+	lblStl = tk.Label(text=u'Output style')
+	rbVar = tk.IntVar()
+	rb1l = tk.Radiobutton(root, value=0, variable=rbVar, text='Artist - Title')
+	rb2l = tk.Radiobutton(root, value=1, variable=rbVar, text='Artist \\n Title')
 
 	stop_flag = False
 	thread = None
